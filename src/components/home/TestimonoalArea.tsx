ed9a5d889b168a38f1015f28dffbde19f232b862
devaraj3032001-max <devaraj3032001@gmail.com>
2026-07-17 00:15:04 +0530

    india emoji update

--- src/components/home/TestimonoalArea.tsx
+++ src/components/home/TestimonoalArea.tsx
@@ -73,7 +73,7 @@ export default function TestimonoalArea() {
                 </div>
                 <div className="testi-des">
                   <h5>sofia_alice</h5>
-                  <span>IN India</span>
+                  <span>🇮🇳 India</span>
                 </div>
               </div>
 
