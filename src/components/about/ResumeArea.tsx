e770f97aede4246ee3671834e3737c2224dc4452
devaraj3032001-max <devaraj3032001@gmail.com>
2026-08-02 17:07:23 +0530

    adsofads

--- src/components/about/ResumeArea.tsx
+++ src/components/about/ResumeArea.tsx
@@ -11,7 +11,7 @@ export default function ResumeArea() {
               <div className="resume-wrapper wow fadeInUp delay-0-2s">
 
                 <div className="resume-box">
-                  <span className="resume-date">2020 - Present</span>
+                  <span className="resume-date">2026 - Present</span>
                   <h2>Creative Designer (Full-Time)</h2>
                   <span>@ YOUBRAND</span>
                   <p>Creative Designer specializing in Social Media Design, Print Design, and Video Editing. Skilled in creating engaging marketing creatives, brand identity assets, promotional materials, and visual content that drive audience engagement, strengthen brand presence, and deliver impactful digital and print communication.</p>
@@ -19,10 +19,10 @@ export default function ResumeArea() {
 
 
                 <div className="resume-box">
-                  <span className="resume-date">2018 - 2020</span>
-                  <h2>CEO & Founder</h2>
-                  <span>@ Thememaster LTD</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <span className="resume-date">2025 - 2026</span>
+                  <h2>Senior Creative Designer</h2>
+                  <span>@ ADSOFADS</span>
+                  <p>Specializing in social media design, print design, and video editing, I create high-impact visual content that enhances brand identity and drives engagement. Experienced in developing creative campaigns, marketing collateral, and digital assets with a strong focus on modern design, branding, and visual storytelling.</p>
                 </div>
 
 
