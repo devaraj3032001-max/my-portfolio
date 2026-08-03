35080061f6075ca6c3068c05d7a58a26b25d5e18
devaraj3032001-max <devaraj3032001@gmail.com>
2026-08-04 00:14:06 +0530

    behance link added

--- src/components/home/BlogArea.tsx
+++ src/components/home/BlogArea.tsx
@@ -29,7 +29,7 @@ export default function BlogArea() {
               <div className="blog-post-caption">
                 <h3>Posted on Jan 26</h3>
                 <h2><a className="link-decoration" href="#">Elanadu – 10 Years of Growth | Anniversary Illustration Carousel</a></h2>
-                <a className="theme-btn theme-btn-two" href="#">Read more <i className="ri-arrow-right-line"></i></a>
+               <a className="theme-btn theme-btn-two" href="https://www.behance.net/gallery/253730681/Elanadus-10-Year-Journey-Anniversary-Illustration?platform=direct" target="_blank" rel="noopener noreferrer">Read more <i className="ri-arrow-right-line"></i></a>
               </div>
             </div>
           </div>
