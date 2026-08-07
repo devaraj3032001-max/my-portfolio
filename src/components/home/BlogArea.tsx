6f388c2974ec03ef10eac83eafb13fa5e6307b40
devaraj3032001-max <devaraj3032001@gmail.com>
2026-08-07 10:50:43 +0530

    Update BlogArea.tsx

--- src/components/home/BlogArea.tsx
+++ src/components/home/BlogArea.tsx
@@ -70,8 +70,8 @@ export default function BlogArea() {
             <div className="col-lg-6">
               <div className="blog-post-caption">
                 <h3>Posted on june 01</h3>
-                <h2><a className="link-decoration" href="https://in.pinterest.com/devarajts30/">Social Media Creatives</a></h2>
-                <a className="theme-btn theme-btn-two" href="https://in.pinterest.com/devarajts30/">See more <i className="ri-arrow-right-line"></i></a>
+                <h2><a className="link-decoration" href="https://in.pinterest.com/devarajts30/_created/">Social Media Creatives</a></h2>
+                <a className="theme-btn theme-btn-two" href="https://in.pinterest.com/devarajts30/_created/">See more <i className="ri-arrow-right-line"></i></a>
               </div>
             </div>
           </div>
