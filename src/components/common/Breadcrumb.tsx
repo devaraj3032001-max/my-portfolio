e4f5133a4c986d07be9d28759ccb01b2ec57a323
devaraj3032001-max <devaraj3032001@gmail.com>
2026-09-21 16:31:26 +0530

    tth sub text

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -13,7 +13,7 @@ export default function Breadcrumb({ title, style_2, style_3, style_4 }: any) {
                 <p>A Collection of My Latest Works and Achievements: Discover the Projects that Define My Passion and Skills</p>
               }
               {style_3 &&
-                <p>Fill out the form below to get in touch with me. I am always excited to hear about new opportunities and I will do my best to respond to your inquiry within 24 hours.</p>
+                <p>MENU CARD DESIGN FOR THE TOWNHOUSE BISTRO CAFÉ — ADSOFADS.</p>
               }
               {style_4 &&
                 <p>Stories, Advice, and Inspiration for the Curious Mind</p>
